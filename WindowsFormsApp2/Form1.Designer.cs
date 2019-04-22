3b4bc878855fbe1f1b4db739e515ed8a292bdcdb
disasterpiece9000 <35543856+disasterpiece9000@users.noreply.github.com>
2019-04-22 15:28:16 -0400

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.numPacketsTxtBox = new System.Windows.Forms.TextBox();
+            this.numPacketsLabel = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.cmbDevices = new System.Windows.Forms.ComboBox();
+            this.adapterLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(374, 25);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 2;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(1070, 703);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 0D;
+            this.gmap.Load += new System.EventHandler(this.gmap_Load);
+            // 
+            // numPacketsTxtBox
+            // 
+            this.numPacketsTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.numPacketsTxtBox.Location = new System.Drawing.Point(127, 463);
+            this.numPacketsTxtBox.Name = "numPacketsTxtBox";
+            this.numPacketsTxtBox.Size = new System.Drawing.Size(100, 39);
+            this.numPacketsTxtBox.TabIndex = 1;
+            this.numPacketsTxtBox.TextChanged += new System.EventHandler(this.NumPacketsTxtBox_TextChanged);
+            // 
+            // numPacketsLabel
+            // 
+            this.numPacketsLabel.AutoSize = true;
+            this.numPacketsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.numPacketsLabel.Location = new System.Drawing.Point(20, 407);
+            this.numPacketsLabel.Name = "numPacketsLabel";
+            this.numPacketsLabel.Size = new System.Drawing.Size(339, 40);
+            this.numPacketsLabel.TabIndex = 2;
+            this.numPacketsLabel.Text = "Number of packets: ";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.startBtn.Location = new System.Drawing.Point(90, 568);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(178, 80);
+            this.startBtn.TabIndex = 3;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // cmbDevices
+            // 
+            this.cmbDevices.FormattingEnabled = true;
+            this.cmbDevices.Location = new System.Drawing.Point(27, 320);
+            this.cmbDevices.Name = "cmbDevices";
+            this.cmbDevices.Size = new System.Drawing.Size(317, 28);
+            this.cmbDevices.TabIndex = 4;
+            this.cmbDevices.SelectedIndexChanged += new System.EventHandler(this.CmbDevices_SelectedIndexChanged);
+            // 
+            // adapterLabel
+            // 
+            this.adapterLabel.AutoSize = true;
+            this.adapterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.adapterLabel.Location = new System.Drawing.Point(105, 263);
+            this.adapterLabel.Name = "adapterLabel";
+            this.adapterLabel.Size = new System.Drawing.Size(153, 40);
+            this.adapterLabel.TabIndex = 5;
+            this.adapterLabel.Text = "Adapter:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1465, 753);
+            this.Controls.Add(this.adapterLabel);
+            this.Controls.Add(this.cmbDevices);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.numPacketsLabel);
+            this.Controls.Add(this.numPacketsTxtBox);
+            this.Controls.Add(this.gmap);
+            this.Name = "Form1";
+            this.Text = "v";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.TextBox numPacketsTxtBox;
+        private System.Windows.Forms.Label numPacketsLabel;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.ComboBox cmbDevices;
+        private System.Windows.Forms.Label adapterLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Collections;
+using GMap.NET.MapProviders;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using PacketDotNet;
+using SharpPcap;
+using System.Net;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        CaptureDeviceList devices;              //List of devices for this computer
+        public static ICaptureDevice device;    //The device being used
+        public static string strPackets = "";          //The data that is captued
+        static int numPackets = 0;
+        static int maxPackets = 0;
+        public static List<Double> latList;
+        public static List<Double> longList;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Get the list of devices
+            devices = CaptureDeviceList.Instance;
+
+            //Make sure at least one device is found
+            if (devices.Count < 1)
+            {
+                MessageBox.Show("No capture devices found! You really messed up this one :(");
+                Application.Exit();
+            }
+
+            //Add devices to the combo box
+            foreach (ICaptureDevice dev in devices)
+            {
+                cmbDevices.Items.Add(dev.Description);
+            }
+
+            //Get the target device and display in combo box
+            device = devices[0];
+            cmbDevices.Text = device.Description;
+
+            //Register the handler function on the 'packet arrival' event
+            device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+
+            //Open the device for capture
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+        }
+
+        private static void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+            var tcpPacket = (TcpPacket)packet.Extract(typeof(TcpPacket));
+
+            if (tcpPacket != null)
+            {
+                var ipPacket = (IPPacket)tcpPacket.ParentPacket;
+                IPAddress srcIp = ipPacket.SourceAddress;
+
+                Debug.Write(Environment.NewLine);
+                Debug.Write("New IP: " + srcIp.ToString());
+                Debug.Write(Environment.NewLine);
+
+                var client = new RestClient("https://ipapi.co/" + srcIp.ToString() + "/json/");
+                var request = new RestRequest()
+                {
+                    Method = Method.GET
+                };
+
+                var response = client.Execute(request);
+                var dict = JsonConvert.DeserializeObject<IDictionary>(response.Content);
+            
+
+                if (dict.Contains("latitude") && dict.Contains("longitude"))
+                {
+                    numPackets++;
+
+                    Object latO = dict["latitude"];
+                    Object lonO = dict["longitude"];
+
+                    latList.Add((double)latO);
+                    longList.Add((double)lonO);
+
+                    Debug.Write(Environment.NewLine);
+                    Debug.Write("New coordinates: " + (double)latO + ", " + (double)lonO);
+                    Debug.Write(Environment.NewLine);
+                }
+            }
+        }
+
+        public void addPins()
+        {
+            Debug.Write("Displaying pins");
+            GMapOverlay overlay = new GMapOverlay("markers");
+
+            for (int i = 0; i < latList.Count; i++)
+            {
+                double lat = latList[i];
+                double lon = longList[i];
+                Debug.Write("Pin at: " + lat + ", " + lon);
+
+                GMapMarker marker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+                    new GMap.NET.PointLatLng(lat, lon),
+                    GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red_dot);
+
+                overlay.Markers.Add(marker);
+            }
+
+            gmap.Overlays.Add(overlay);
+        }
+
+        private void gmap_Load(object sender, EventArgs e)
+        {
+            gmap.MapProvider = GoogleMapProvider.Instance;
+            gmap.Manager.Mode = AccessMode.ServerOnly;
+            gmap.Position = new PointLatLng(55.755786121111, 37.617633343333);
+            gmap.ShowCenter = false;
+            gmap.CanDragMap = true;
+            gmap.MarkersEnabled = true;
+            gmap.MinZoom = 1;
+            gmap.MaxZoom = 20;
+            gmap.Zoom = 1;
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (startBtn.Text == "Start")
+            {
+                string filter = "ip and tcp";
+                device.Filter = filter;
+                device.StartCapture();
+                startBtn.Text = "Stop";
+                Debug.Write("Start");
+            }
+
+            else
+            {
+                device.StopCapture();
+                startBtn.Text = "Start";
+                maxPackets = Convert.ToInt32(numPacketsTxtBox.Text);
+            }
+        }
+
+        private void CmbDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            device = devices[cmbDevices.SelectedIndex];
+            cmbDevices.Text = device.Description;
+
+            //Register the handler function on the 'packet arrival' event
+            device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+
+            //Open the device for capture
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+        }
+
+        private void NumPacketsTxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (numPackets >= maxPackets)
+            {
+                Debug.Write("Done");
+                device.StopCapture();
+                startBtn.Text = "Start";
+                addPins();
+            }
+        }
+    }
+}
